5b8d14fd922f35200ac448eb187cb75f4e58fec7
ishansharma9525 <ishansharmamdh@gmail.com>
2026-06-28 16:31:16 +0530

    second commit

--- ExpenseTracking/Board/models.py
+++ ExpenseTracking/Board/models.py
@@ -7,7 +7,7 @@ class Account(models.Model):
     name = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=10, decimal_places=2)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    expense_list = models.ManyToManyField('Expense', blank=True)
+    # expense_list = models.ManyToManyField('Expense', blank=True)
 
 class Expense(models.Model):
     name = models.CharField(max_length=100)
@@ -20,14 +20,22 @@ class Expense(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='expenses')
 
 
+    def save(self, *args, **kwargs):
+        if self.long_term:
+            self.monthly_expenses = self.calculate_monthly_expenses()
+        super(Expense, self).save(*args, **kwargs)
+
+
     def calculate_monthly_expenses(self):
         if self.long_term:
             if self.interest_rate == 0:
-                return self.amount / ((self.end_date - self.date)/ 30)
+                days = (self.end_date - self.date).days
+                months = days / 30
+                return self.amount / months
             else:
                 months = (self.end_date.year - datetime.now().year) * 12 + self.end_date.month - datetime.now().month
                 monthly_rate = self.interest_rate / 12 /100
                 monthly_expense = (self.amount * monthly_rate) / (1 - (1 + monthly_rate) ** -months)
                 return round(monthly_expense, 2)
         else:
-            return self.monthly_expense
+            return self.monthly_expenses
--- ExpenseTracking/Board/views.py
+++ ExpenseTracking/Board/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import userCreationForm
+# from exptracker import models
+from .models import Account, Expense
 # Create your views here.
